d7dea163ce17963bca322483ca7771f8557bcb5d
MasterBlaster3 <josephmaifeld@gmail.com>
2022-05-18 14:06:58 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const newMug of clay) {
+   const mug = `${newMug} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
+
+
